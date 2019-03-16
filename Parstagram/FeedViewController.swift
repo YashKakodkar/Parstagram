ce0efd61ac27fcdc75189f0fb163736549a43406
Yash Kakodkar <ymac@yashs-macbook-pro.local>
2019-03-16 01:31:19 -0500

    part 2

--- Parstagram/FeedViewController.swift
+++ Parstagram/FeedViewController.swift
@@ -46,6 +46,18 @@ class FeedViewController: UIViewController,UITableViewDelegate, UITableViewDataS
         
     }
     
+    @IBAction func onLogoutButton(_ sender: Any) {
+        PFUser.logOut()
+        
+        let main = UIStoryboard(name: "Main", bundle: nil)
+        let loginViewController = main.instantiateViewController(withIdentifier: "LoginViewController")
+        let delegate = UIApplication.shared.delegate as! AppDelegate
+        delegate.window?.rootViewController = loginViewController
+        
+    }
+    
+    
+    
     // Implement the delay method
     func run(after wait: TimeInterval, closure: @escaping () -> Void) {
         let queue = DispatchQueue.main
@@ -102,6 +114,23 @@ class FeedViewController: UIViewController,UITableViewDelegate, UITableViewDataS
         
         return cell
         
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let post = posts[indexPath.row]
+        let comment  = PFObject(className: "Comments")
+        comment["text"] = "This is a dummy comment"
+        comment["post"] = post
+        comment["author"] = PFUser.current()!
+        
+        post.add(comment, forKey: "comments")
+        post.saveInBackground{ (success,error) in
+            if(success){
+                print("Comment saved")
+            }else{
+                print("Error saving comment")
+            }
+        }
     }
 
     /*
